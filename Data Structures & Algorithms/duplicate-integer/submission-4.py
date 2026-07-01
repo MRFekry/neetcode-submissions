51f9cd6895cea6f5f80372694ae2de53ca188e48
Mohamed Ramadan Fekry <mfekry.ra@gmail.com>
2026-07-02 00:01:07 +0300

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        dic = dict()
+        for num in nums:
+            if num in dic:
+                return True
+            dic[num] = None
+        return False
+
