a5ecb5d359aca11598e3b18f840e9c3af0232052
Mohamed Ramadan Fekry <mfekry.ra@gmail.com>
2026-07-02 15:53:20 +0300

    Add: is-anagram - submission-18

---
+++ Data Structures & Algorithms/is-anagram/submission-18.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        d = {}
+        for c in s:
+            d[c] = d.get(c, 0) + 1
+        for c in t:
+            d[c] = d.get(c, 0) - 1
+        for v in d.values():
+            if v != 0:
+                return False
+        
+        return True
+        
