fd4acf0f350c9ffd20b610e441ae949fc68c0d2b
Mohamed Ramadan Fekry <mfekry.ra@gmail.com>
2026-07-03 13:30:21 +0300

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        x_index = 0
+        for x in nums:
+            y_index = 0
+            for y in nums:
+                if x_index == y_index:
+                    y_index = y_index + 1
+                    continue
+                if x + y == target:
+                    return [x_index, y_index]
+                y_index = y_index + 1
+            x_index = x_index + 1
