ff1f9a43fc3913b85c415b25e9e1b6a709da697d
Mohamed Ramadan Fekry <mfekry.ra@gmail.com>
2026-07-16 16:58:02 +0200

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+            if len(nums) == 0: return []
+            if len(nums) == 1: return [nums[0]]
+            if k == 1: return nums
+            res = []
+            for i, n in enumerate(nums):
+                rp = i + k
+                while rp <= len(nums):
+                    res.append(max(nums[i:rp]))
+                    break
+                if rp == len(nums):
+                    break
+            return res
+
+
